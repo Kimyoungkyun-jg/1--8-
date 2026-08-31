2c99908c21ffbebc391b85260a852b7069cc3b2f
신용준 <4rest528@gmail.com>
2026-08-31 17:45:43 +0900

    base code

---
+++ week1team8/ShaderW0.hlsl
@@ -0,0 +1,34 @@
+cbuffer constants : register(b0) // FConstants
+{
+    float3 Offset;
+    float Pad1;
+    float3 Scale;
+    float Pad2;
+}
+
+struct VS_INPUT
+{
+    float4 position : POSITION;
+    float4 color : COLOR;
+};
+
+struct PS_INPUT
+{
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+};
+
+PS_INPUT mainVS(VS_INPUT input) // Vertex Shader
+{
+    PS_INPUT output;
+    
+    output.position = input.position * float4(Scale, 1.0) + float4(Offset, 0);
+    output.color = input.color;
+    
+    return output;
+}
+
+float4 mainPS(PS_INPUT input) : SV_TARGET // Pixel Shader
+{
+    return input.color;
+}
