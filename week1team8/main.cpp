7531603c7ba9e31383f8f2d75c7e581c7d58e0da
신용준 <4rest528@gmail.com>
2026-09-01 16:30:53 +0900

    충돌 해결

--- week1team8/main.cpp
+++ week1team8/main.cpp
@@ -133,16 +133,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 
 	//테스트용
-	// AObstacle* block = SpawnColider<AObstacle>(FVector(0, -10, 0), EPrimitive::Rectangle, true, { 0.5,0.5,0.5 });
-	// block->SetVelocity(0);
+	AObstacle* block = SpawnColider<AObstacle>(FVector(0, -10, 0), EPrimitive::Rectangle, true, { 0.5,0.5,0.5 });
+	block->SetVelocity(0);
 
-	//ACollider* NewBall = SpawnColider<ACollider>(FVector(0, 0, 0), EPrimitive::Circle, true, { 0.1, 0.1, 1 });
-	// ACollider* ball = SpawnColider<ACollider>(FVector(0, 0, 0), EPrimitive::Circle, true, { 1, 1, 1 });
-	ACollider* rec = SpawnColider<ACollider>(FVector(0, 0, 0), EPrimitive::Rectangle, true, { 1, 1, 1 });
+	ACollider* NewBall = SpawnColider<ACollider>(FVector(0, 0, 0), EPrimitive::Circle, true, { 0.5, 0.5, 1 });
 
 	// Main Loop (Quit Message가 들어오기 전까지 아래 Loop를 무한히 실행하게 됨)
 	ASlingShot *SlingShot = SpawnColider<ASlingShot>({ -0.5, -1.0, 0 }, EPrimitive::Rectangle, false, { 0.05, 0.3, 1 });
-	ABird * Bird = SpawnColider<ABird>({ -0.5, -0.5, 0 }, EPrimitive::Circle, false, { 0.05, 0.05, 0.1 });
+	ABird * Bird = SpawnColider<ABird>({ -0.5, -0.5, 0 }, EPrimitive::Circle, false, { 0.2, 0.2, 0.1 });
 
 	SlingShot->EquippedBird = Bird;
 	SlingShot->ShotPoint = Bird->GetLocation();
